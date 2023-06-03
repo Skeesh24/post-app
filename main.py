3f2cc332046807817f4103a61e034f408cf91e9e
Skeesh24 <d.pacooloff@gmail.com>
2023-06-03 17:10:08 +0500

    handle post not found case and replying with constant codes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+from fastapi import Response
 
 
 class Post(BaseModel):
@@ -16,7 +17,7 @@ temp_storage = [{"title": "default title",
                  "content": "default content", "id": 1}]
 
 
-def find_post(id: int):
+def find_post(id: int) -> dict | None:
     for post in temp_storage:
         if post["id"] == id:
             return post
@@ -25,17 +26,17 @@ def find_post(id: int):
 app = FastAPI()
 
 
-@app.get("/")
+@ app.get("/")
 async def root():
     return {"message ": "welcome to my api"}
 
 
-@app.get("/posts")
+@ app.get("/posts")
 async def get_posts():
     return {"data": temp_storage}
 
 
-@ app.post("/posts")
+@ app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0, 1000000)
@@ -43,13 +44,16 @@ async def create_post(post: Post):
     return {"data": post_dict}
 
 
-@app.get("/posts/latest")
+@ app.get("/posts/latest")
 async def get_latest_post():
     post = temp_storage[len(temp_storage)-1]
     return {"detail": post}
 
 
-@app.get("/posts/{id}")
-async def get_post(id: int):
+@ app.get("/posts/{id}")
+async def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            f"the {id}th post was not found")
     return {"data": post}
