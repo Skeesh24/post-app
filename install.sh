4c19cb847ff53aeb17a19f3de90661b83c6cb14c
Skeesh24 <d.pacooloff@gmail.com>
2023-06-21 14:35:11 +0500

    install script was modified

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 apt install python3-venv &&
 apt install python3-pip
-"/bin/python3" -m pip install -U fastapi[all] &&
-"/bin/python3" -m pip install -U mysql &&
-"/bin/python3" -m pip install -U mysql-connector-python &&
-"/bin/python3" -m pip install -U sqlalchemy &&
-"/bin/python3" -m pip install -U bcrypt &&
-"/bin/python3" -m pip install -U passlib &&
-"/bin/python3" -m pip install -U python-jose[cryptography] &&
-"/bin/python3" -m pip install -U alembic &&
-"/bin/alembic" init && 
-"/bin/alembic" upgdare 9714d366efca
+apt install mysql-server
+apt install libmysqlclient-dev
+python3 -m pip install -U fastapi[all] &&
+python3 -m pip install -U sqlalchemy &&
+python3 -m pip install -U bcrypt &&
+python3 -m pip install -U passlib &&
+python3 -m pip install -U python-jose[cryptography] &&
+python3 -m pip install -U alembic &&
+python3 -m pip install -U mysql-connector-python &&
+python3 -m pip install -U mysql &&
+python3 -m pip install -U uvicorn
