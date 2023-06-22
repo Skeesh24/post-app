14a2afc2d3f74155ae9e2d6b2b3ed75dcdd59817
Skeesh24 <d.pacooloff@gmail.com>
2023-06-22 18:46:38 +0500

    moved latest endpoint into root router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
 from uvicorn import run
+from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.classes.database import get_db
+from app.classes.models import User
+from app.classes.oauth2 import get_current_user
+from app.classes.schemas.posts import post_response
+from app.classes.schemas.users import user_response
+from app.classes.schemas.votes import vote_create
+from app.classes.models import posts, Vote, votes
 
 from app.routers.post import router as post_router
 from app.routers.user import router as user_router
@@ -29,6 +38,27 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message ": "welcome post-app"}
+
+
+@app.get("/latest", response_model=post_response)
+async def get_latest_post(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    res = db.execute(posts.select().order_by(posts.c["created_at"]).where(
+        posts.c["user_id"] == user.id)).fetchall()
+    if res is None or len(res) == 0:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            "you don't added any posts yet")
+    # reverse and taking the one
+    res = res[::-1][0]
+
+    post_dict = dict(zip(res._fields, res.tuple()))
+
+    upvotes = db.execute(votes.select().where(
+        votes.c.user_id == user.id).where(votes.c.post_id == res.id)).fetchall()
+
+    post_dict.update({"creator": user_response(**user.__dict__)})
+    post_dict.update({"upvotes_count": len(upvotes)})
+
+    return post_dict
 
 app.include_router(post_router)
 app.include_router(user_router)
